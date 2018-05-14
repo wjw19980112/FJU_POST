75742778a56708b92146cec02b9e7560f181b627
wjw19980112 <wjw19980112@gmail.com>
2018-05-14 22:33:12 +0800

    1.Change and set icon 2.Add the Start page

--- app/src/main/java/com/example/lawrence/fju_post/MainActivity.java
+++ app/src/main/java/com/example/lawrence/fju_post/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.lawrence.fju_post;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mHandler.sendEmptyMessageDelayed(GOTO_MAIN_ACTIVITY, 2000);
     }
+    private static final int GOTO_MAIN_ACTIVITY = 0;
+    private Handler mHandler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+
+            switch (msg.what) {
+                case GOTO_MAIN_ACTIVITY:
+                    Intent intent = new Intent();
+                    //將原本Activity的換成MainActivity
+                    intent.setClass(MainActivity.this, StudentPage.class);
+                    startActivity(intent);
+                    finish();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+    };
 }
