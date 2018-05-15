192b8e00b03922474f49a31be9d3c9f79145bfeb
Yuan Liang <1191299684@qq.com>
2018-05-15 19:09:12 +0800

    连接到login界面

--- app/src/main/java/com/example/lawrence/fju_post/StudentPage.java
+++ app/src/main/java/com/example/lawrence/fju_post/StudentPage.java
@@ -2,15 +2,13 @@ package com.example.lawrence.fju_post;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Handler;
-import android.content.Intent;
 
 public class StudentPage extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student_page);
+        setContentView(R.layout.activity_login);
     }
 
 }
