a595379e9ae729ebc2e805aec4a832d9d588ba6d
Yuan Liang <1191299684@qq.com>
2018-06-01 22:15:21 +0800

    整合,加上登出

--- app/src/main/java/com/example/lawrence/fju_post/MeFragment.java
+++ app/src/main/java/com/example/lawrence/fju_post/MeFragment.java
@@ -1,18 +1,79 @@
 package com.example.lawrence.fju_post;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MeFragment extends Fragment {
+    private static final String TAG= "MeFragment";
+
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private Button logout;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View  view =  inflater.inflate(R.layout.fragment_me, container, false);
+
+
+        logout=(Button) view.findViewById(R.id.button3);
+
+        setupFirebaseListener();
+
+        logout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Log.d(TAG,"onClick: attempting to sign out the user.");
+                FirebaseAuth.getInstance().signOut();
+            }
+        });
+
         return view;
     }
+
+    private void setupFirebaseListener(){
+        Log.d(TAG,"setupFirebaseListener: setting up the auth state listener.");
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    Log.d(TAG,"onAuthStateChanged: signed_in:" + user.getUid());
+                }else{
+                    Log.d(TAG,"onAuthStateChanged: signed_out:" );
+                    Toast.makeText(getActivity(),"Signed_out",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getActivity(),LoginActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        if(mAuthStateListener != null) {
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuthStateListener);
+        }
+    }
+
+
 }
